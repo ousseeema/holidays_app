d0f5e91834138ea6fc3cc10ec0d0df9d145580da
ousseeema <oussama.ferchichi01@gmail.com>
2024-08-04 21:33:16 +0100

    updates

---
+++ lib/view/info.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class infoPage extends StatefulWidget {
+  const infoPage({super.key});
+
+  @override
+  State<infoPage> createState() => _infoPageState();
+}
+
+class _infoPageState extends State<infoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const ;
+  }
+}
